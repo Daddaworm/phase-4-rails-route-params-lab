d419d1444891db4bdc72162181b87ad6ab38716d
Ethan Rodriguez <ethan.rodriguez@mac.com>
2021-07-27 23:09:24 -0400

    Completed assignment

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,26 @@
+
+
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
-    render json: students
+    # binding.pry
+    if (params[:first_name]).present?
+      first_name = Student.find_by(first_name: params[:first_name])
+      render json: first_name
+    elsif (params[:last_name]).present?
+      last_name = Student.find_by(last_name: params[:last_name])
+      render json: last_name
+    else
+      students = Student.all
+      render json: students
+    end
   end
+
+
+  def show
+    student = Student.find(params[:id])
+    render json: student
+  end
+
 
 end
